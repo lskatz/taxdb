2de144d65a5bcdcc7f9f4e7aa027d2d095cc2d6b
Lee Katz - Aspen <gzu2@cdc.gov>
2019-06-13 13:59:34 -0400

    query by sci name

--- scripts/taxdb_query.pl
+++ scripts/taxdb_query.pl
@@ -13,14 +13,17 @@ exit(main());
 
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help mode=s sep|separator=s outdir=s taxon|taxa=s@)) or die $!;
+  GetOptions($settings,qw(help name|scientificName=s@ mode=s sep|separator=s outdir=s taxon|taxa=s@)) or die $!;
   $$settings{outdir} ||= './out';
   $$settings{sep} ||= "\t";
   $$settings{mode}||= "default";
   $$settings{mode} = lc($$settings{mode});
 
   die usage() if(!@ARGV || $$settings{help});
-  die "ERROR: need --taxon\n".usage() if(!$$settings{taxon});
+
+  if(!$$settings{taxon} && !$$settings{name}){
+    die "ERROR: need either --name or --taxon\n".usage();
+  }
 
   my ($dbpath)=@ARGV;
 
@@ -35,9 +38,22 @@ sub main{
   my @descendent = ();
   my @topLevelTaxon = (); # user-specified taxids
 
+  # Separate out the taxa into an array @topLevelTaxon.
   for my $taxlist(@{ $$settings{taxon} }){
     for my $taxid(split(/,/, $taxlist)){
       push(@topLevelTaxon, $taxid);
+    }
+  }
+  
+  # Translate names to taxids here, and push them
+  # onto @topLevelTaxon
+  for my $nameList(@{$$settings{name}}){
+    for my $name(split(/,/, $nameList)){
+      my $taxid = findTaxonByScientificName($name, $dbh, $settings);
+      if(scalar(@$taxid) > 1){
+        print STDERR "WARNING: found multiple taxids for $name\n";
+      }
+      push(@topLevelTaxon, @$taxid);
     }
   }
 
@@ -116,13 +132,38 @@ sub dumpLineage{
   return 1;
 }
 
-      
+sub findTaxonByScientificName{
+  my($name, $dbh, $settings) = @_;
+
+  my @taxid;
+
+  my $sth = $dbh->prepare(qq(
+    SELECT NODE.tax_id
+    FROM NODE LEFT JOIN NAME on NODE.tax_id = NAME.tax_id
+    WHERE NAME.name_class = "scientific name"
+    AND name_txt = ?
+  ))
+    or die "ERROR preparing SELECT statement: ".$dbh->errstr();
+
+  my $res = $sth->execute($name)
+    or die "ERROR executing SELECT statement for $name: ".$dbh->errstr();
+
+  while(my @row = $sth->fetchrow_array()){
+    push(@taxid, $row[0]);
+  }
+   
+  return \@taxid;
+}
 
 sub usage{
   "Display the details on any given taxon
   Usage: $0 [options] file.sqlite3
   --taxon     Taxon IDs.  Can specify multiple --taxon or
               comma-separate them, e.g., --taxon 1,2,3
+  --name      A scientific name. Quote it if there
+              are spaces, e.g., 'Listeria monocytogenes'.
+              Multiple names can be specified in the same
+              way as --taxon.
   --separator By default, columns are tab-separated.
   --mode      Either: default, lineage
   "
--- t/05_query_Listeria_monocytogenes.t
+++ t/05_query_Listeria_monocytogenes.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 3;
+use Test::More tests => 4;
 use Data::Dumper;
 
 my $taxdb = "data/Listeria-2019-06-12.sqlite";
@@ -20,5 +20,8 @@ is $sciNameLineArr[14], "Listeria monocytogenes", "Found the scientific name for
 my $lineage = join("\t", qw(1639  1637    186820  1385    91061   1239    1783272 2       131567  1));
 my $obsLineage = `perl scripts/taxdb_query.pl --taxon 1639 $taxdb --mode lineage 2>data.tmp/query.log`;
 chomp($obsLineage);
-is $lineage, $obsLineage, "Lineage for Listeria monocytogenes";
+is $lineage, $obsLineage, "Lineage for Listeria monocytogenes, by tax_id";
 
+my $obsLineage2 = `perl scripts/taxdb_query.pl --name "Listeria monocytogenes" $taxdb --mode lineage 2>data.tmp/query.log`;
+chomp($obsLineage2);
+is $lineage, $obsLineage2, "Lineage for Listeria monocytogenes, by scientific name";
